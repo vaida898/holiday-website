ac9939fa2ce046f7c53ecfdea8ea3e88a5c3983a
Ubuntu <ubuntu@ip-172-31-95-182.ec2.internal>
2019-12-16 00:26:19 +0000

    Added google maps API into bootstrap grid system

---
+++ js/maps.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+}
